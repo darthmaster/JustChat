dc290b427fbbc5bdb33ca9413cfb1aa8617d31e1
Albert S <fkmthn2012@gmail.com>
2020-01-17 12:02:15 +0300

    Magic

---
+++ JustChatServer/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace JustChat
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string username) 
+        {
+            await Clients.All.SendAsync("Receive", message, username);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.UserIdentifier} врывается в тред!");
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.UserIdentifier} покинул нас. Press F to pay respect.");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ JustChatServer/Startup.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace JustChat
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR().AddHubOptions<ChatHub>(optinos => { optinos.EnableDetailedErrors = true; });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {            
+                endpoints.MapHub<ChatHub>("/chat", options => 
+                {
+                    options.ApplicationMaxBufferSize = 1024;
+                    options.TransportMaxBufferSize = 1024;
+
+                });
+            });
+        }
+    }
+}
