285a40a20c00d83f89aa510b367e26411368bfbd
Albert S <fkmthn2012@gmail.com>
2020-01-20 14:18:58 +0300

    хуй

--- JustChatServer/ChatHub.cs
+++ JustChatServer/ChatHub.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,19 +8,15 @@ namespace JustChat
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string username,string message) 
-        {
-            await Clients.All.SendAsync("Receive", username, message);
-            
-        }
+        public async Task Send(string username, string message) => await Clients.All.SendAsync("Receive", username, message);
         public override async Task OnConnectedAsync()
         {
-            await Clients.All.SendAsync("Receive", $"{Context.UserIdentifier} врывается в тред!");
+            await Clients.All.SendAsync("Receive", string.Empty, $"{Context.ConnectionId} врывается в тред!");
             await base.OnConnectedAsync();
         }
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            await Clients.All.SendAsync("Receive", $"{Context.UserIdentifier} покинул нас. Press F to pay respect.");
+            await Clients.All.SendAsync("Receive", string.Empty, $"{Context.ConnectionId} покинул нас. Press F to pay respect.");
             await base.OnDisconnectedAsync(exception);
         }
     }
